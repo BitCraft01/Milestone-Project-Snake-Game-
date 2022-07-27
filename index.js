2401a4a29fe279e77cb9b816a38f3815c5b64781
Bachir <bachirmdicko@gmail.com>
2022-07-27 13:22:31 -0400

    Added a score counter

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ let row = 20
 let column = 20
 let context
 
+let gameOver = false;
+let score = 0
+function scoreCount(){
+    score++
+    const scoreNum = document.querySelector("#scoreNum")
+    scoreNum.innerHTML = score
+
+
+}
+
+
 //Function to generate grid
 window.onload = () => {
     grid = document.getElementById("grid")
@@ -22,19 +33,29 @@ window.onload = () => {
             https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect */
 //Function to keep redrawing the canvas
 function update(){
+    if(gameOver){
+        return;
+    }
     context.fillStyle = "black"
     context.fillRect(0, 0, grid.width, grid.height)
 
     context.fillStyle ="red"
     context.fillRect(treatX, treatY, gridSquare, gridSquare)
     if(snakeX === treatX && snakeY === treatY){
+        
         snakeBody.push([treatX, treatY])
+        
         treatPosition()
+        scoreCount()
+        
     }
     for(let i = snakeBody.length - 1; i > 0; i--) {
+        
         snakeBody[i] = snakeBody[i - 1];
+        
     }if(snakeBody.length){
         snakeBody[0] = [snakeX, snakeY]
+        
     }
 
     context.fillStyle = "Lime"
@@ -45,7 +66,16 @@ function update(){
         context.fillRect(snakeBody[i][0], snakeBody[i][1], gridSquare, gridSquare)
     }
 
-    
+    //Collision detection
+    if(snakeX < 0 || snakeX > column * gridSquare || snakeY < 0 || snakeY > row * gridSquare){
+        gameOver = true;
+        alert("Game Over")
+    }
+    for(let i = 0; i < snakeBody.length; i++){
+        if(snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]){
+            alert("Game Over")
+        }
+    }
 }
 
 //Generating the snake
@@ -62,6 +92,8 @@ let speedY = 0
 
 //function to randomize treat position
 function treatPosition(){
+    
     treatX = Math.floor(Math.random() * column) * gridSquare
     treatY = Math.floor(Math.random() * row) * gridSquare
+    
 }
